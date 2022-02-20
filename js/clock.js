af6ffbe4ee31d196732191178e758537ca205a96
wsson0823 <wooson0823@gmail.com>
2022-02-20 22:41:54 +0900

    수정

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,30 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("#clock");
+const clockampm = document.querySelector("#ampm");
 
-function getclock(){
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2,"0");
-    const minutes = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
-
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+function getClock() {
+	const date = new Date();
+	let ampm;
+	let hours;
+	const minutes = String(date.getMinutes()).padStart(2, 0);
+	const seconds = String(date.getSeconds()).padStart(2, 0);
+	if (date.getHours() < 12) {
+		ampm = "AM";
+		if (date.getHours() == 0) {
+			hours = 12;
+		} else {
+			hours = String(date.getHours()).padStart(2, 0);
+		}
+	} else {
+		ampm = "PM";
+		if (date.getHours() == 12) {
+			hours = 12;
+		} else {
+			hours = String(date.getHours() - 12).padStart(2, 0);
+		}
+	}
+	clockampm.innerHTML = `${ampm}`;
+	clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
-getclock();
-setInterval(getclock, 1000);
+
+getClock();
+setInterval(getClock, 1000);
